cfbdb18134167f1d73c09bd41f62f3c2304e2d94
David Rajkumar <rajkumar26.jd@gmail.com>
2018-05-28 22:45:12 +0100

    My First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace SOD_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] files = { @"C:\Users\Janu\Desktop\stereo-pairs\" };
+
+            foreach (string file in files)
+            {
+                Bitmap leftImage = (Bitmap)System.Drawing.Image.FromFile(file + @"\imL.png");
+                Bitmap rightImage = (Bitmap)System.Drawing.Image.FromFile(file + @"\imR.png");
+
+                Bitmap RangeImage = RangeMap.RangeMapGenerator(leftImage, rightImage);
+
+                RangeImage.Save(file + @"\imD.png");
+            }
+        }
+    }
+}
---
+++ RangeMap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SOD_System
+{
+    class RangeMap
+    {
+        public static Bitmap RangeMapGenerator(Bitmap leftImage, Bitmap rightImage)
+        {
+
+            Bitmap rangeBitmap = new Bitmap(leftImage.Width, leftImage.Height);
+
+            for (int x = 2; x < leftImage.Width - 2; x++)
+            {
+                for (int y = 2; y < leftImage.Height - 2; y++)
+                {
+
+                    int sad = 0;
+
+                    for (int m = -2; m < 2; m++)
+                    {
+                        for (int n = -2; n < 2; n++)
+                        {
+                            sad += Math.Abs(leftImage.GetPixel(x + m, y + n).ToArgb() - rightImage.GetPixel(x + m, y + n).ToArgb());
+                        }
+                    }
+
+                    rangeBitmap.SetPixel(x, y, Color.FromArgb(calcualteRange(sad) * 2000000000));
+                }
+            }
+
+            return rangeBitmap;
+        }
+
+
+        public static int calcualteRange(int sad)
+        {
+            double baseLine = 0.176, focalLength = 887, range;
+
+            range = (40 * baseLine * focalLength / sad) * 10000;
+
+            return (int)range;
+        }
+
+    }
+}
